3c294e21037c8732446e161a36e31a9371c8a850
JuhaePark <43876312+JuhaePark@users.noreply.github.com>
2026-06-19 11:30:12 -0400

    Create example_run.sh

---
+++ example_run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+source $your_conda_environment
+conda activate polyply 
+conda info | grep " active environment"
+source $your_gromacs_environment
+
+POLYMER="PQT12"
+N=20
+replica="A"
+
+#initialization
+if [ -f "system_${POLYMER}_melt_n${N}.top" ] ; then
+  polyply gen_coords -p system_${POLYMER}_melt_n${N}.top -o tmp_${POLYMER}_melt_n${N}_${replica}.gro -dens 600 -name ${POLYMER}
+else
+  echo "ERROR!! File system_${POLYMER}_melt_n${N}.top NOT found. Exiting..."
+  exit
+fi
+
+# Min and eq                                                                                                                                               
+if [ ! -f "${POLYMER}_melt_n${N}_${replica}.gro" ] ; then
+  gmx grompp -normvsbds -p system_${POLYMER}_melt_n${N}.top -c starting_packing.gro -f ../M3_mdps/martini_v3.x_em.mdp -o 1-min
+  gmx mdrun -v -deffnm 1-min -nt 2                                                                                                                      
+  gmx grompp -normvsbds -p system_${POLYMER}_melt_n${N}.top -c 1-min.gro -f ../M3_mdps/martini_v3.x_NPT_ramping.mdp -o 2-NPT -maxwarn 2
+  gmx mdrun -v -deffnm 2-NPT -nt 2
+fi
+
+mv 2-NPT.gro ${POLYMER}_melt_n${N}_${replica}.gro
+echo 0 | gmx trjconv -f 2-NPT.xtc -o 2-NPT-whole.xtc -pbc whole -s 2-NPT.tpr
+rm tmp_${POLYMER}_melt_n${N}_${replica}.gro 1-min.??? 2-NPT.??? mdout.mdp 2-NPT_prev.cpt \#*
